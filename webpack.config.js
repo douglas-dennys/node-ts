3a6cccdac7bdd469478d6c41649c9687627407ba
Douglas Dennys <douglasdennys45@gmail.com>
2019-08-29 18:56:47 -0300

    alter folder build project

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var config = {
   output: {
     libraryTarget: "commonjs",
     filename: "index.js",
-    path: path.resolve(__dirname, "../dist")
+    path: path.resolve(__dirname, "./dist")
   },
   resolve: {
     extensions: [".ts", ".js"]
