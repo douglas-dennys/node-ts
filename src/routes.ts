9a4219a157b8c3d0a91fc8c5fe87f33d175dd48c
Douglas Dennys <douglasdennys45@gmail.com>
2019-08-29 13:31:20 -0300

    alter endpoint status

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,11 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) =>
-  res.json({ message: "Welcome to Service Express.JS" })
+  res.json({
+    code: 200,
+    data: "Welcome to Service Express.JS",
+    message: `response in host ${process.env.APP_HOST} successfully`
+  })
 );
 
 export default router;
